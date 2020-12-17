6a9dc043514efa217e327bf082ca6b0dcbecbaee
Jason Ng <2334439391@qq.com>
2020-12-17 17:43:47 +0800

    響應式設計

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// toTopBtn的設置
 let toTopBtn = document.querySelector(".toTop")
 window.addEventListener("scroll", () => {
     let st = document.documentElement.scrollTop || document.body.scrollTop
@@ -10,3 +11,20 @@ window.addEventListener("scroll", () => {
 toTopBtn.addEventListener("click",()=>{
     document.documentElement.scrollTo({ left: 0, top: 0, behavior: 'smooth' })
 })
+
+//界面的開關
+document.querySelector(".burger").addEventListener("click",function(){
+    this.classList.toggle("open")
+    const header=document.querySelector("header")
+    header.classList.toggle("openMenu")
+    const openMenuA=document.querySelectorAll(".openMenu nav a")
+    openMenuA.forEach(e=>{
+        e.addEventListener("click",()=>{
+            header.classList.remove("openMenu")
+            this.classList.remove("open")
+        })
+    })
+})
+
+
+
